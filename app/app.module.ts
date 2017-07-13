de9441e3f50aee1acee92f31527b2d1366c2b13e
franJer <ortega.franciscojavier@gmail.com>
2017-07-13 10:48:56 +0200

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SaludoComponent} from './saludo.componet';
@@ -8,6 +9,9 @@ import { ControlComponent } from './control/control.component';
 import { AddRemoveDomComponent } from './add-remove-dom/add-remove-dom.component';
 import { UseNgForComponent } from './use-ng-for/use-ng-for.component';
 import { TwowaybindingComponent } from './twowaybinding/twowaybinding.component'
+import { RestcountriesService} from './restcountries.service';
+import { CountryComponent } from './country/country.component';
+
 
 @NgModule({
   declarations: [
@@ -16,13 +20,15 @@ import { TwowaybindingComponent } from './twowaybinding/twowaybinding.component'
     ControlComponent,
     AddRemoveDomComponent,
     UseNgForComponent,
-    TwowaybindingComponent
+    TwowaybindingComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [RestcountriesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
